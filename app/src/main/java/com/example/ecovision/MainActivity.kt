3c134b0887ba16d79d5d52fa331a164827918320
mafazer <fauziwelcome@gmail.com>
2024-06-04 17:45:12 +0700

    adding fragment and bottom navigation

--- app/src/main/java/com/example/ecovision/MainActivity.kt
+++ app/src/main/java/com/example/ecovision/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.example.ecovision
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.menu.getItem(2).isEnabled = false
+
+        val homeFragment = HomeFragment()
+        val articleFragment = ArticleFragment()
+        val historyFragment = HistoryFragment()
+        val profileFragment = ProfileFragment()
+
+        setCurrentFragment(homeFragment)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.miHome -> setCurrentFragment(homeFragment)
+                R.id.miArticle -> setCurrentFragment(articleFragment)
+                R.id.miHistory -> setCurrentFragment(historyFragment)
+                R.id.miProfile -> setCurrentFragment(profileFragment)
+            }
+            true
         }
+
     }
+
+    private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.frameFragment, fragment)
+            commit()
+        }
 }
