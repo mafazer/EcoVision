093aa237f4707b2377bbe3aabf21581bf00b1d61
mafazer <fauziwelcome@gmail.com>
2024-06-16 18:26:05 +0700

    add tips of the day database

--- app/src/main/java/com/example/ecovision/HomeFragment.kt
+++ app/src/main/java/com/example/ecovision/HomeFragment.kt
@@ -6,20 +6,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.ecovision.databinding.ActivityMainBinding
 import com.example.ecovision.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
+    private lateinit var tipsData: TipsData
+
     private var _binding: FragmentHomeBinding? = null
-    // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment using view binding
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -27,16 +26,18 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Set up click listener using view binding
         binding.imageViewQuestionMark.setOnClickListener {
             val intent = Intent(requireContext(), MapsActivity::class.java)
             startActivity(intent)
         }
+
+        tipsData = TipsData()
+        val tipOfTheDay = tipsData.getTipForToday()
+        binding.textViewTip.text = tipOfTheDay
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        // Nullify the binding when the view is destroyed
         _binding = null
     }
 }
